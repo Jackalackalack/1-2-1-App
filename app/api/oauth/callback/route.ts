6cb102aecd9807a29cb2047eaec8e0ead5f869a6
Jackalackalack <Jack.c.abraham@gmail.com>
2026-09-16 22:19:24 +0100

    Update route.ts

--- app/api/oauth/callback/route.ts
+++ app/api/oauth/callback/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { getSetupOAuthClient } from "@/lib/google";
+export const dynamic = "force-dynamic"; 
 
 export async function GET(req: Request) {
   if (process.env.GOOGLE_REFRESH_TOKEN) {
